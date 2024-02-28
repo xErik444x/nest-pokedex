8e468f55b5f124c01eac5413ac1878d743c055d3
xErik444x <erikschwerdt18@gmail.com>
2024-02-28 02:35:12 -0300

    axios adapter & http adater

--- README.md
+++ README.md
@@ -20,7 +20,10 @@ npm i -g @nestjs/cli
 ```
 docker-compose up -d
 ```
-
+5. Opcional, llenar la base de datos
+```
+localhost:3000/api/v2/seed
+```
 ## Stacks
 * MongoDB
 * Nest
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,23 +1,33 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 @Injectable()
 export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
   async executeSeed() {
-    const resp = await fetch(
-      ' https://pokeapi.co/api/v2/pokemon?limit=10&offset=0',
-    );
     try {
-      const parserResp: PokeResponse = await resp.json();
-      const pokemons: CreatePokemonDto[] = parserResp.results.map(
-        ({ name, url }) => {
-          const segmentsUrl = url.split('/');
-          const no = +segmentsUrl[segmentsUrl.length - 2];
-          return { name, no };
-        },
+      const resp: PokeResponse = await this.http.get(
+        ' https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0',
       );
-      return pokemons;
+      const pokemons: CreatePokemonDto[] = resp.results.map(({ name, url }) => {
+        const segmentsUrl = url.split('/');
+        const no = +segmentsUrl[segmentsUrl.length - 2];
+        return { name, no };
+      });
+      await this.pokemonModel.deleteMany();
+      const res = await this.pokemonModel.insertMany(pokemons);
+      const insertedCount = res.filter((result) => result != null).length;
+      return `Added ${insertedCount} pokemons to db!`;
     } catch (error) {
+      console.log(`🐧 -> executeSeed -> error:`, error);
       throw new InternalServerErrorException('Fetch error.');
     }
   }
